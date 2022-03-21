ba71305791999b4babd2f650458c5655d3a38429
jorsel <jorgen.selsoyvoll@gmail.com>
2022-03-21 23:19:24 +0100

    added svg for dark mode. Added TODO to readme

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Advice generator app
 
-Advice generator app built with the provided frontend mentor design.
-Advice is being fetched from the [advice slip api](https://api.adviceslip.com/)
+Advice generator app built with the provided frontend mentor design. <br />
+Advice is being fetched from the [advice slip api](https://api.adviceslip.com/) <br />
+
+https://jorsel.github.io/advice-generator-app/ <br />
+
+# TODO
+App is caching in firefox, making it only get advice once. <br />
+App is misshapen in Internet Explorer <br />
--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
-function getAdvice(){
+function getAdvice() {
     let promise = fetch("https://api.adviceslip.com/advice");
     promise.then(response => {
         response.json()
-        //json object has the name of "slip", as in advice slip.
-        .then(data =>{
-            console.log(data.slip);
-            document.getElementById('advice_id').innerText ="Advice# " + data.slip.id;
-            document.getElementById('advice_text').innerText = data.slip.advice;
-        })
-    })  
+            //json object has the name of "slip", as in advice slip.
+            .then(data => {
+                console.log(data.slip);
+                document.getElementById('advice_id').innerText = "Advice# " + data.slip.id;
+                document.getElementById('advice_text').innerText = data.slip.advice;
+            })
+    })
 }
+
+
 
 /* firefox caches this page,
  have open dev tools while testing or use another browser.
