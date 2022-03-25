98cf148ec488d545ed9a2348927487a078f2c80a
jorsel <jorgen.selsoyvoll@gmail.com>
2022-03-25 20:46:16 +0100

    fixed resizing issue for web and mobile

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getAdvice() {
-    let promise = fetch("https://api.adviceslip.com/advice", {cache: 'no-store'});
+    let promise = fetch("https://api.adviceslip.com/advice", {cache: 'no-store'}); //to avoid caching issue
     promise.then(response => {
         response.json()
             //json object has the name of "slip", as in advice slip.
